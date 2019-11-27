a745c80b2825831f851b52e036c11759b7b609fe
EthelSotoA <57779240+EthelSotoA@users.noreply.github.com>
2019-11-27 00:19:51 -0600

    Proyecto

---
+++ BellayEleganteJoyera/app/src/main/java/com/example/bellayelegantejoyera/Anillos.java
@@ -0,0 +1,13 @@
+package com.example.bellayelegantejoyera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Anillos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anillos);
+    }
+}
--- BellayEleganteJoyera/app/src/main/java/com/example/bellayelegantejoyera/TarjetaDebito.java
+++ BellayEleganteJoyera/app/src/main/java/com/example/bellayelegantejoyera/TarjetaDebito.java
@@ -5,7 +5,6 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
